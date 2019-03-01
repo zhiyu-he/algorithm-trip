dfa6365462ae5b6c7afd51faf1d72d99b57bc6c0
hezhiyu <hezhiyu@bytedance.com>
2019-03-01 18:51:09 +0800

    check
    Change-Id: I20d414543876e4ed2d0dd130fcbcd786116193dd

---
+++ growth/oj/leet_code/algorithms/98-validate-binary-search-tree.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+# https://zh.wikipedia.org/wiki/%E4%BA%8C%E5%85%83%E6%90%9C%E5%B0%8B%E6%A8%B9
+
+class Solution(object):
+    def isValidBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+    
