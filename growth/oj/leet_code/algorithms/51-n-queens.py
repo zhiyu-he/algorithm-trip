d8532dd9f18cb1f5a07e2c4417c2eab4ea3c8546
hezhiyu <hezhiyu@bytedance.com>
2019-03-21 14:41:26 +0800

    [leetcode/alg] AC: 51-n-queens.py
    Change-Id: I5f3d961bd0db6fc1b22ae311c2ed70749a3a04c5

---
+++ growth/oj/leet_code/algorithms/51-n-queens.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+
+class Solution(object):
+    def solveNQueens(self, n):
+        """
+        :type n: int
+        :rtype: List[List[str]]
+        """
+        global_list = []
+        self.slove(n, global_list, [], 0, set(), set(), set())
+        return self.gen_pic(global_list, n)
+
+    def slove(self, n, global_list, l, cur_row, pie, cols, nai):
+        if cur_row == n:
+            global_list.append(l[:])
+            return
+        start_col = 0
+        while start_col < n:
+            point = (cur_row, start_col)
+            if point not in pie and point not in cols and point not in nai:
+                l.append(start_col)
+                self.add_conflict(n, point, pie, cols, nai)
+                self.slove(n, global_list, l, cur_row + 1, pie, cols, nai)
+                self.remove_conflict(n, point, pie, cols, nai)
+                l.pop(len(l)-1 )
+            start_col += 1
+        return
+
+    def gen_pic(self, answer_list, n):
+        pic_list = []
+        for each_answer in answer_list:
+            pic = []
+            for pos in each_answer:
+                s = '.' * pos + 'Q' + '.' * (n-pos-1)
+                pic.append(s)
+            pic_list.append(pic)
+        return pic_list
+
+    def add_conflict(self, n, point, pie, cols, nai):
+        row_i = point[0]
+        col_j = point[1]
+
+        sum_i_j = row_i + col_j
+        next_row = row_i + 1
+        while next_row < n and sum_i_j - next_row >= 0:
+            pie.add((next_row, sum_i_j - next_row))
+            next_row += 1
+
+        next_row = row_i + 1
+        div_i_j = row_i - col_j
+        while next_row < n and next_row - div_i_j < n:
+            nai.add((next_row, next_row - div_i_j))
+            next_row += 1
+
+        next_row = row_i + 1
+        while next_row < n:
+            cols.add((next_row, col_j))
+            next_row += 1
+
+    def remove_conflict(self, n, point, pie, cols, nai):
+        row_i = point[0]
+        col_j = point[1]
+
+        sum_i_j = row_i + col_j
+        next_row = row_i + 1
+        while next_row < n and sum_i_j - next_row >= 0:
+            pie.remove((next_row, sum_i_j - next_row))
+            next_row += 1
+
+        next_row = row_i + 1
+        div_i_j = row_i - col_j
+        while next_row < n and next_row - div_i_j < n:
+            nai.remove((next_row, next_row - div_i_j))
+            next_row += 1
+
+        next_row = row_i + 1
+        while next_row < n:
+            cols.remove((next_row, col_j))
+            next_row += 1
+
+
+if __name__ == '__main__':
+    obj = Solution()
+    print obj.solveNQueens(4)
