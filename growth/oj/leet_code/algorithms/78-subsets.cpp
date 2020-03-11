dcae060675d94c231c5f9c0ab7aa55fa1fa591f9
hezhiyu <hezhiyu@BYTEDANCE.COM>
2020-03-11 09:22:53 +0800

    [leetcode/alg] AC: 78-subsets.cpp and 90-subsets-ii v2
    Change-Id: If384ef58de81c10f5b50e37d11f882814250c02b

--- growth/oj/leet_code/algorithms/78-subsets.cpp
+++ growth/oj/leet_code/algorithms/78-subsets.cpp
@@ -16,15 +16,34 @@ class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
         sort(nums.begin(), nums.end());
-        vector<vector<int>> res;
         vector<int> cur;
-        //set<int> indexSet;
-        //dfs(res, cur, nums, indexSet, 0);
-        //res.push_back(nums);
-        dfs2(res, cur, nums, 0);
 
+        vector<vector<int>> res;
+        res.push_back(cur);
+        
+        int sameLen = 1;
+        int lastPush = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (i == 0 || nums[i-1] != nums[i]) {
+                sameLen = 1;
+            } else {
+                sameLen += 1;
+            }
+            int n = res.size();
+            int j = 0;
+            if (sameLen > 1) {
+                j = n - lastPush;
+            }
+            lastPush = 0;
+            for (; j < n; j++) {
+                res.push_back(res[j]);
+                res.back().push_back(nums[i]);
+                lastPush += 1;
+            }
+        }
         return res;
     }
+
 
     // 考虑input被处理到哪里了
     void dfs2(vector<vector<int>>& res, vector<int> cur, vector<int>& input, int curIdx) {
@@ -32,12 +51,17 @@ public:
             res.push_back(cur);
             return;
         }
-
         // pick or no-pick
         dfs2(res, cur, input, curIdx+1);
+        if (cur.size() == 0 && curIdx > 0 && input[curIdx-1] == input[curIdx]) {
+            cout << "cur_idx: " << curIdx << endl;
+            return;
+        }
+
         cur.push_back(input[curIdx]);
         dfs2(res, cur, input, curIdx+1);
     }
+
 
     // 这种dfs考虑的是已处理的元素个数
     void dfs(vector<vector<int>>& res, vector<int>& cur, vector<int>& input, set<int>& indexSet, int cur_start) {
@@ -63,7 +87,7 @@ public:
 
 int main() {
     Solution s;
-    vector<int>  vec{1,2,1};
+    vector<int>  vec{1,2,2};
     
     auto a = s.subsets(vec);
     for (auto v : a) {
