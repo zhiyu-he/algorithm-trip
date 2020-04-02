0ce7b34ebc35d2ca559846e95acd8e7b6effd2fc
hezhiyu <hezhiyu@BYTEDANCE.COM>
2020-04-02 15:31:38 +0800

    [leetcode/alg] AC: 136-single-number.cpp DONE: O(n)
    Change-Id: Ia107181f1734cb68a7a67f1ded1fbfed90c95124

--- growth/oj/leet_code/algorithms/136-single-number.cpp
+++ growth/oj/leet_code/algorithms/136-single-number.cpp
@@ -8,22 +8,10 @@ using namespace std;
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        if (nums.size() == 1)
-            return nums[0];
-        sort(nums.begin(), nums.end());
-        if (nums[0] != nums[1])
-            return nums[0];
-        if (nums[nums.size()-1] != nums[nums.size()-2])
-            return nums[nums.size()-1];
-        int cnt = 0;
-        for(int i = 0; i < nums.size() - 2; i += 2)
-            if (nums[i] != nums[i+1]) {
-                if (nums[i-1] == nums[i])
-                    return nums[i+1];
-                else
-                    return nums[i];
-            }
-        return -1;
+        int num = 0;
+        for (int i = 0; i < nums.size(); i++)
+            num ^= nums[i];
+        return num;
     }
 };
 
