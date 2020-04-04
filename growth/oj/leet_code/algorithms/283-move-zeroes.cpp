6ea7a0454e7c41497183f76afa43534949bbf023
hezhiyu <hezhiyu@BYTEDANCE.COM>
2020-04-04 23:19:44 +0800

    [leetcode/alg] AC: 283-move-zeroes.cpp DONE: note
    Change-Id: I3b3ebd587adc374324c3fe781ed7b4690fa01677

--- growth/oj/leet_code/algorithms/283-move-zeroes.cpp
+++ growth/oj/leet_code/algorithms/283-move-zeroes.cpp
@@ -6,6 +6,7 @@ using namespace std;
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
+        // think about overwrite not swap
         int j = 0;
         for (int i = 0; i < nums.size(); i++) {
             if (nums[i] != 0) {
