5c14e2b96a9c86e45882ab19bf9a8fb5a1cca4bc
hezhiyu <hezhiyu@BYTEDANCE.COM>
2020-04-25 11:49:40 +0800

    [lt/alg] OPT: 876-middle-of-the-linked-list.cpp O(N)
    Change-Id: I50d0b88f69050eaa5ad0342f94ba4a6f430653ab

--- growth/oj/leet_code/algorithms/876-middle-of-the-linked-list.cpp
+++ growth/oj/leet_code/algorithms/876-middle-of-the-linked-list.cpp
@@ -19,6 +19,7 @@ struct ListNode {
 
 class Solution {
 public:
+    // method1
     ListNode* middleNode(ListNode* head) {
         int cnt = 0;
         ListNode* start = head;
@@ -30,6 +31,18 @@ public:
         int mid = cnt /2;
         for (int i = 0; i < mid; i++) {
             res = res->next;
+        }
+        return res;
+    }
+    // method2
+    ListNode* middleNode2(ListNode* head) {
+        ListNode* res = head;
+        int len = 1;
+        while(head != NULL) {
+            if (len % 2 == 0)
+                res = res->next;
+            head = head->next;
+            len += 1;
         }
         return res;
     }
