b87d036a3eaa4c3c60e2ee3134dbe9fafcacb404
hezhiyu <hezhiyu@bytedance.com>
2020-05-06 22:07:25 +0800

    tmp
    Change-Id: Idf257b183e1e631929d786f8d991ec16ea39fed7

---
+++ growth/oj/leet_code/algorithms/169-majority-element.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        vector<int> tmp(nums.size());
+        return 0;
+    }
+
+    void merge_sort(vector<int>& nums, vector<int>& tmp) {
+
+    }
+
+
+};
+
+int main() {
+    return 0;
+}
