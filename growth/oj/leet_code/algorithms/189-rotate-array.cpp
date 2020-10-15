41357a21f565709d5f2468adc6581081f86f7ac4
zhiyu-he <miracle.sniper@gmail.com>
2020-10-16 00:19:04 +0800

    [lt/alg] AC: 189-rotate-array.cpp
    Change-Id: I5bc005f5b4a5be5e02118cd8943e572b99004aa3

--- growth/oj/leet_code/algorithms/189-rotate-array.cpp
+++ growth/oj/leet_code/algorithms/189-rotate-array.cpp
@@ -2,6 +2,28 @@
 #include<vector>
 
 using namespace std;
+
+class Solution2 {
+public:
+    void rotate(vector<int>& nums, int k) {
+        if (nums.size() == 0 || nums.size() == 1) return;
+        k %= nums.size();
+        reverse(nums, 0, nums.size()-1);
+        reverse(nums, k, nums.size()-1);
+        reverse(nums, 0, k-1);
+    }
+
+    void reverse(vector<int>& nums, int start, int end) {
+        while(start < end) {
+            int tmp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = tmp;
+            start++;
+            end--;
+        }
+    }
+};
+
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
