b600cb4c80c6791a1998392d2b4c3cd964767148
zhiyu-he <miracle.sniper@gmail.com>
2020-11-28 20:19:36 +0800

    [lt/alg] AC: 376-wiggle-subsequence.cpp
    Change-Id: I084372245f26ebc2a5ea8b022b9b9b376c4eb820

---
+++ growth/oj/leet_code/algorithms/376-wiggle-subsequence.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+class Solution {
+public:
+    int wiggleMaxLength(vector<int>& nums) {
+        if (nums.size() == 0) return 0;
+        int res = 0;
+        vector<vector<int>> cache(nums.size()+1, vector<int>(3));
+        for (int i = 0; i < nums.size(); i++) {
+            res = max(res, wiggle(nums, i, 0, cache));
+            res = max(res, wiggle(nums, i, 1, cache));
+        }
+        return res;
+    }
+    int wiggle(vector<int>& nums, int start_idx, int direct, vector<vector<int>>& cache) {
+        if (start_idx == nums.size()-1) return 1;
+        if (cache[start_idx][direct] != 0) return cache[start_idx][direct];
+        int len = 1;
+        for (int i = start_idx+1; i < nums.size(); i++) {
+            // up
+            if (direct == 0 && nums[start_idx] < nums[i]) {
+                len = max(len, 1 + wiggle(nums, i, 1, cache));
+            }
+            // down
+            if (direct == 1 && nums[start_idx] > nums[i]) {
+                len = max(len, 1 + wiggle(nums, i, 0, cache));
+            }
+        }
+        cache[start_idx][direct] = len;
+        return len;
+    }
+};
+
+
+
+int main() {
+    Solution s;
+    return 0;
+}
