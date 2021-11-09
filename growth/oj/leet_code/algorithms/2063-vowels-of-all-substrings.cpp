cd91f45dd46e9a7d942ba6d896fa8df923108543
hezhiyu <hezhiyu@bytedance.com>
2021-11-09 09:24:19 +0800

    MOD: change path
    Change-Id: I6abebb3c3472d28f1c640a225f586d7aaa0caf91

--- growth/oj/leet_code/algorithms/2063-vowels-of-all-substrings.cpp
+++ growth/oj/leet_code/algorithms/2063-vowels-of-all-substrings.cpp
@@ -10,6 +10,8 @@ public:
         for (int i = 0; i < word.size(); i++) {
             if (target.find(word[i]) == string::npos)
                 continue;
+            // 本质上我们构造了一个字符串 [0, i-1] [i] [i+1, n]
+            // 对于任意一个如上的字符串，i - 0 + 1种选法，后面则有 (n - i - 1 + 1)种选法
             res += (i - 0 + 1) * (word.size() - i);
         }
         return res;
