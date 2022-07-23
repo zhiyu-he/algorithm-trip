560925b0db5c51b07a9820709a049f6041ae76da
hezhiyu <hezhiyu@bytedance.com>
2022-07-23 14:35:26 +0800

    [lt/alg] OPT: 86-partition-list.cpp
    Change-Id: I652c068e9e630ca9b12c2b491fb3ffb8a2834a10

--- growth/oj/leet_code/algorithms/86-partition-list.cpp
+++ growth/oj/leet_code/algorithms/86-partition-list.cpp
@@ -53,6 +53,42 @@ public:
     }
 };
 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution2 {
+public:
+    ListNode* partition(ListNode* head, int x) {
+        if (head == NULL) return head;
+        ListNode* small = new ListNode(-1);
+        ListNode* large = new ListNode(-1);
+        ListNode* sh = small;
+        ListNode* lh = large;
+        while(head) {
+            if (head->val < x) {
+                small->next = head;
+                small = small->next;
+                head = head->next;
+                small->next = NULL;
+            } else {
+                large->next = head;
+                large = large->next;
+                head = head->next;
+                large->next = NULL;
+            }
+        }
+        small->next = lh->next;
+        return sh->next;
+    }
+};
+
 int main() {
 	Solution s;
 	return 0;
