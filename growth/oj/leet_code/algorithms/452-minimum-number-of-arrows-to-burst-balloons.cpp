5d4acabe916a451bdd51fe20a12bd782b79ce508
00ooheoo00 <miracle.sniper@gmail.com>
2023-01-06 00:12:55 +0800

    [lt/alg] UPT: 452-minimum-number-of-arrows-to-burst-balloons.cpp
    Change-Id: I35cd08672602e0773e4dac6b5ae8021b85498963

--- growth/oj/leet_code/algorithms/452-minimum-number-of-arrows-to-burst-balloons.cpp
+++ growth/oj/leet_code/algorithms/452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -28,6 +28,37 @@ public:
     }
 };
 
+class Solution2 {
+public:
+    int findMinArrowShots(vector<vector<int>>& points) {
+        sort(points.begin(), points.end(), [](vector<int>& a, vector<int>& b) {
+            return a[1] == b[1] ? a[0] < b[0] : a[1] < b[1];
+        });
+        int nums = 1;
+        int left = points[0][0], right = points[0][1];
+        for (int i = 1; i < points.size(); i++) {
+            if (right < points[i][0]) {
+                left = points[i][0];
+                right = points[i][1];
+                nums += 1;
+            } else {
+                int l = points[i][0], r = points[i][1];
+                if (left < l && r < right) {
+                    left = l;
+                    right = r;
+                } else if (left < l) {
+                    left = l;
+                } else if (r < right) {
+                    right = r;
+                } else {
+                    
+                }
+            }
+        }
+        return nums;
+    }
+};
+
 
 int main() {
     return 0;
